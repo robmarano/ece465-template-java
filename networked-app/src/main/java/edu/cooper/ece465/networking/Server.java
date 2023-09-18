76416403dc730add7ffd47bfe89a68eb69831f97
Rob Marano <robmarano@gmail.com>
2023-09-18 19:23:59 -0400

    Adding networked-app client/server example

---
+++ networked-app/src/main/java/edu/cooper/ece465/networking/Server.java
@@ -0,0 +1,26 @@
+package edu.cooper.ece465.networking;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java DistributedImagingServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+                new MyDistributedThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
